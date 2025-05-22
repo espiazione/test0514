5b53fcbea3494141ba261e47de86a093ee9a0847
espiazione <j61086118@gmail.com>
2025-05-22 18:49:32 +0800

    Update and rename 1_page🙀.py to 1_pageNDVI.py

---
+++ pages/1_pageNDVI.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import ee
+from google.oauth2 import service_account
+import geemap.foliumap as geemap
+
+# 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
+service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
+
+# 使用 google-auth 進行 GEE 授權
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
+    scopes=["https://www.googleapis.com/auth/earthengine"]
+)
+
+# 初始化 GEE
+ee.Initialize(credentials)
+
+###############################################
+
+st.set_page_config(layout="wide")
+st.title("🌍 20250514 作業：GEE Streamlit App 練習")
+st.write("Harmonized Sentinel-2 MSI: MultiSpectral Instrument, Level-1C 土地覆蓋 K-means 分10群")
+
+# 地理區域
+my_point = ee.Geometry.Point([121.462129, 25.108993])
+
+# 擷取 Harmonized Sentinel-2 MSI: MultiSpectral Instrument, Level-1C 衛星影像
+my_image = (
+    ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
+    .filterBounds(my_point)
+    .filterDate('2021-01-01', '2022-01-01')
+    .sort('CLOUDY_PIXEL_PERCENTAGE')
+    .first()
+    .select('B.*')
+)
+
+vis_params = {'min':100, 'max': 3500, 'bands': ['B8',  'B4',  'B3']}
+
+my_lc = ee.Image('ESA/WorldCover/v200/2021')
+# ESA WorldCover 10m v200
+# https://developers.google.com/earth-engine/datasets/catalog/ESA_WorldCover_v100#bands
+
+classValues = [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100]
+remapValues = ee.List.sequence(0, 10)
+label = 'lc'
+my_lc = my_lc.remap(classValues, remapValues, bandName='Map').rename(label).toByte()#把剛剛的數列0~10儲存格式變8位元
+
+classVis = {
+  'min': 0,
+  'max': 10,
+  'palette': ['006400' ,'ffbb22', 'ffff4c', 'f096ff', 'fa0000', 'b4b4b4',
+            'f0f0f0', '0064c8', '0096a0', '00cf75', 'fae6a0']
+}
+# 顯示地圖
+my_Map = geemap.Map()
+my_Map.centerObject(my_image.geometry(), 10)
+my_Map.to_streamlit(height=600)
