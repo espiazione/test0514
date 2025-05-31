6bfbb3c2cfcc94a6eafcc70b22111e24415d63bc
espiazione <j61086118@gmail.com>
2025-05-31 16:35:44 +0800

    Update 1_簡述發展變化.py

--- pages/1_簡述發展變化.py
+++ pages/1_簡述發展變化.py
@@ -7,9 +7,7 @@ st.title("應用程式主頁")
 
 st.title("利用擴充器示範")
 
-
-
 with st.expander("播放mp4檔"):
-    video_file = open("https://drive.google.com/file/d/16Ma2OM8vdAoQHKDqdUVBTXvy27emtL6_/view?usp=drive_link", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
+    video_file = open("ndwi_timelapse.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
     video_bytes = video_file.read()
     st.video(video_bytes)
