1174f5405559ec96b86771d50703d0d24f70fc2c
espiazione <j61086118@gmail.com>
2025-05-31 16:05:08 +0800

    Update and rename 2_.py to 1_簡述發展變化.py

---
+++ pages/1_簡述發展變化.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from datetime import date
+
+st.set_page_config(layout="wide", page_title="彰化台中沿海發展變化")
+
+st.title("應用程式主頁")
+
+st.title("利用擴充器示範")
+
+
+
+with st.expander("播放mp4檔"):
+    video_file = open("ndwi_timelapse.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
+    video_bytes = video_file.read()
+    st.video(video_bytes)
